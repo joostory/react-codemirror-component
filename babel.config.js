5eedd28ca08e5a9717a20f15377b80b1aaded06c
joo.yoon <joo.yoon@kakaocorp.com>
2018-12-02 11:37:00 +0900

    babel build

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = (api) => {
+    api.cache(true)
+  
+    return {
+      presets: [
+        "@babel/env",
+        "@babel/react"
+      ],
+      plugins: [
+        ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ]
+    }
+  };
+  
